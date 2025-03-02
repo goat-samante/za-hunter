3631d0c8d80225dac032b0adb966965b80931729
Samuel Amante <amantsa76@gmail.com>
2025-03-02 11:40:16 -0600

    Initial Commit

---
+++ za hunter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  za hunter
+//
+//  Created by Samuel Amante on 2/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
