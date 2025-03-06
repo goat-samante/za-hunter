4206c29ae87d04cd5f59e5b8b843cee2c9af65ff
Samuel Amante <amantsa76@gmail.com>
2025-03-05 19:16:04 -0600

    Added Website and Directions

--- LocationDetailsView.swift
+++ LocationDetailsView.swift
@@ -10,13 +10,31 @@ import MapKit
 struct LocationDetailsView: View {
     let mapItem: MKMapItem
     var body: some View {
+        let address = mapItem.placemark.subThoroughfare! + " " +
+        mapItem.placemark.thoroughfare! + "\n" +
+        mapItem.placemark.locality! + ", " +
+        mapItem.placemark.administrativeArea! + " " +
+        mapItem.placemark.postalCode!
         VStack {
-            
-            let address = mapItem.placemark.subThoroughfare! + " " +
-            let address = mapItem.placemark.thoroughfare! + "\n" +
-            let address = mapItem.placemark.locality! + ", " +
-            let address = mapItem.placemark.administrativeArea! + " " +
-            let address = mapItem.placemark.postalCode
+            Text(mapItem.placemark.name!).font(.title).bold()
+            Text(address).padding()
+            Text(mapItem.phoneNumber!).padding()
+            if let url = mapItem.url {
+                Link(destination: url, label: {
+                    Text("Website").padding()
+                })
+            }
+            Button(action: {
+                let latitude = mapItem.placemark.coordinate.latitude
+                let longitude = mapItem.placemark.coordinate.longitude
+                let url = URL(string: "maps://?saddr=&daddr=\(latitude),\(longitude)")
+                if UIApplication.shared.canOpenURL(url!) {
+                    UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                }
+            }, label: {
+                Text("Directions")
+            })
+            Spacer()
         }
     }
 }
